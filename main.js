b771c0b6cf0297baa8cc9f0243bae511d49cd221
pedrodias070707 <112518875+pedrodias070707@users.noreply.github.com>
2023-03-13 19:00:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("meu primeiro projeto no console");
+var first_name = "Pedro";
+var last_name = "Dias";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var adicao = a+b;
+console.log(adicao);
+
+var subtracao = a-b;
+console.log(subtracao);
+
+var multiplicacao = a*b;
+console.log(multiplicacao);
+
+var divisao = a/b;
+console.log(divisao);
